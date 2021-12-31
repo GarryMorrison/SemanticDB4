751bb3fa70d7a386743646aecbd984e109c7e5fb
Garry Morrison <garry@semantic-db.org>
2021-12-31 13:48:28 +1100

    Added PoweredOperator.

---
+++ src/Operator/PoweredOperator.cpp
@@ -0,0 +1,52 @@
+
+//
+// Created by Garry Morrison on 13/09/2020.
+//
+
+#include "PoweredOperator.h"
+
+Sequence PoweredOperator::Compile(NewContext& context, const Sequence& seq) const {
+    Sequence result(seq);
+    for (unsigned int i = 0; i < pow_; i++) {
+        result = b_op->Compile(context, result);  // Memory leak??
+    }
+    return result;
+}
+
+Sequence PoweredOperator::Compile(ContextList& context, const Sequence& seq) const {
+    Sequence result(seq);
+    for (unsigned int i = 0; i < pow_; i++) {
+        result = b_op->Compile(context, result);  // Memory leak??
+    }
+    return result;
+}
+
+Sequence PoweredOperator::Compile(ContextList& context, const Sequence& seq, const Ket& label_ket) const {
+    Sequence result(seq);
+    for (unsigned int i = 0; i < pow_; i++) {
+        result = b_op->Compile(context, result, label_ket);  // Memory leak??
+    }
+    return result;
+}
+
+Sequence PoweredOperator::Compile(ContextList& context, const Sequence& seq, const Ket& label_ket, const Ket& multi_label_ket) const {
+    Sequence result(seq);
+    for (unsigned int i = 0; i < pow_; i++) {
+        result = b_op->Compile(context, result, label_ket, multi_label_ket);
+    }
+    return result;
+}
+
+Sequence PoweredOperator::Compile(ContextList& context, const Sequence& seq, const Ket& label_ket, const std::vector<Sequence>& args) const {
+    Sequence result(seq);
+    for (unsigned int i = 0; i < pow_; i++) {
+        result = b_op->Compile(context, result, label_ket, args);
+    }
+    return result;
+}
+
+const std::string PoweredOperator::to_string() const {
+    std::string s;
+    s = b_op->to_string() + "^" + std::to_string(pow_);
+    return s;
+}
---
+++ src/Operator/PoweredOperator.h
@@ -0,0 +1,36 @@
+#pragma once
+
+//
+// Created by Garry Morrison on 13/09/2020.
+//
+
+
+#include <string>
+#include <memory>
+#include "BaseOperator.h"
+#include "NumericOperator.h"
+
+class PoweredOperator : public BaseOperator {
+private:
+    // BaseOperator* b_op;
+    std::shared_ptr<BaseOperator> b_op;
+    unsigned int pow_;
+
+public:
+    // PoweredOperator(BaseOperator* base_op, const unsigned int power) {b_op = base_op; pow_ = power; };
+    PoweredOperator(std::shared_ptr<BaseOperator> base_op, const unsigned int power) { b_op = base_op; pow_ = power; };
+    Sequence Compile(NewContext& context, const Sequence& seq) const;
+    Sequence Compile(ContextList& context, const Sequence& seq) const;
+    // Sequence Compile(ContextList& context, const Sequence& seq, const ulong label_idx) const;
+    // Sequence Compile(ContextList& context, const Sequence& seq, const ulong label_idx, const ulong multi_label_idx) const;
+    // Sequence Compile(ContextList& context, const Sequence& seq, const ulong label_idx, const std::vector<Sequence>& args) const;
+
+    Sequence Compile(ContextList& context, const Sequence& seq, const Ket& label_ket) const;
+    Sequence Compile(ContextList& context, const Sequence& seq, const Ket& label_ket, const Ket& multi_label_ket) const;
+    Sequence Compile(ContextList& context, const Sequence& seq, const Ket& label_ket, const std::vector<Sequence>& args) const;
+
+    const int type() const { return OPPOWER; }
+    const ulong get_head_op_idx() const { return 0; }
+    const std::shared_ptr<BaseOperator> get_tail_op() const { return std::make_shared<NumericOperator>(1); }
+    const std::string to_string() const;
+};
