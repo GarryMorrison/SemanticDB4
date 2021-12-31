421d87c27442d26a5842adb7c1eaa7e5e79bf373
Garry Morrison <garry@semantic-db.org>
2021-12-31 14:28:38 +1100

    Added IfElseStatement.

---
+++ src/Sequence/IfElseStatement.cpp
@@ -0,0 +1,74 @@
+//
+// Created by Garry Morrison on 29/05/2021.
+//
+
+#include "IfElseStatement.h"
+
+
+const size_t IfElseStatement::size() const {
+    return if_rules.size() + else_rules.size();  // Not sure the best way to return size here.
+}
+
+const std::string IfElseStatement::to_string() const {
+    std::string s = "if( " + condition.to_string() + " ):" + if_rules.to_string("    ");
+    if (else_rules.size() > 0) { s += "    else:" + else_rules.to_string("    "); }
+    return s + "    end:";
+}
+
+const std::string IfElseStatement::to_string(const std::string& prefix) const {
+    std::string s = prefix + "if( " + condition.to_string() + " ):" + if_rules.to_string(prefix + "    ");  // prefix + "    " or "    " + prefix?
+    if (else_rules.size() > 0) { s += prefix + "    else:" + else_rules.to_string(prefix + "    "); }
+    return s + prefix + "    end:";
+}
+
+
+Sequence IfElseStatement::Compile(ContextList& context) const {
+    // std::cout << "IfElseStatement::Compile(context):\n";
+    std::string condition_str = condition.Compile(context).to_ket().label();
+    if (condition_str == "yes") {
+        return if_rules.Compile(context);
+    }
+    else {
+        return else_rules.Compile(context);
+    }
+    return Sequence();
+}
+
+Sequence IfElseStatement::Compile(ContextList& context, const Ket& label_ket) const {
+    // std::cout << "IfElseStatement::Compile(context, label_ket):\n";
+    std::string condition_str = condition.Compile(context, label_ket).to_ket().label();
+    if (condition_str == "yes") {
+        return if_rules.Compile(context, label_ket);
+    }
+    else {
+        return else_rules.Compile(context, label_ket);
+    }
+    return Sequence();
+}
+
+Sequence IfElseStatement::Compile(ContextList& context, const Ket& label_ket, const Ket& multi_label_ket) const {
+    // std::cout << "IfElseStatement::Compile(context, label_ket, multi_label_ket):\n";
+    std::string condition_str = condition.Compile(context, label_ket, multi_label_ket).to_ket().label();
+    if (condition_str == "yes") {
+        return if_rules.Compile(context, label_ket, multi_label_ket);
+    }
+    else {
+        return else_rules.Compile(context, label_ket, multi_label_ket);
+    }
+    return Sequence();
+}
+
+Sequence IfElseStatement::Compile(ContextList& context, const Ket& label_ket, const std::vector<Sequence>& args) const {
+    // std::cout << "IfElseStatement::Compile(context, label_ket, args):\n";
+    // Sequence result = condition.Compile(context, label_ket, args);
+    // std::cout << "    condition: " << result.to_string() << "\n";
+    std::string condition_str = condition.Compile(context, label_ket, args).to_ket().label();
+    if (condition_str == "yes") {
+        return if_rules.Compile(context, label_ket, args);
+    }
+    else {
+        return else_rules.Compile(context, label_ket, args);
+    }
+    return Sequence();
+}
+
---
+++ src/Sequence/IfElseStatement.h
@@ -0,0 +1,53 @@
+#pragma once
+
+//
+// Created by Garry Morrison on 29/05/2021.
+//
+
+
+// #define IFELSESTATEMENT 13
+
+#include "BaseSequence.h"
+#include "OperatorWithSequence.h"
+#include "MultiLearnRule.h"
+
+
+class IfElseStatement : public BaseSequence {
+private:
+    OperatorWithSequence& condition;
+    MultiLearnRule& if_rules;
+    MultiLearnRule& else_rules;
+
+public:
+    // IfElseStatement(OperatorWithSequence &our_condition, MultiLearnRule &if_rules) : condition(our_condition), if_vec_rules(if_rules) {
+    //     else_vec_rules = MultiLearnRule();
+    // };
+    IfElseStatement(OperatorWithSequence& condition2, MultiLearnRule& if_rules2, MultiLearnRule& else_rules2) : condition(condition2), if_rules(if_rules2), else_rules(else_rules2) {};
+
+    // void append(const LearnRule& learn_rule);
+    // void append(const OperatorWithSequence& operator_with_sequence);
+
+    // methods needed to support BaseSequence:
+    const int type() const { return IFELSESTATEMENT; }
+    const bool is_ket() const { return false; }
+    const size_t size() const;
+    const std::string to_string() const;
+    const std::string to_string(const std::string& prefix) const;
+
+    Ket to_ket() const { return Ket(); }  // Do something more interesting with these later!
+    Superposition to_sp() const { return Superposition(); }
+    Sequence to_seq() const { return Sequence(); }
+
+    std::shared_ptr<BaseSequence> b_add(std::shared_ptr<BaseSequence> bSeq) { return bSeq; }  // Maybe change later!
+    std::shared_ptr<BaseSequence> b_append(std::shared_ptr<BaseSequence> bSeq) { return bSeq; }
+
+    Sequence Compile(ContextList& context) const;  // needs filling out
+    // Sequence Compile(ContextList& context, const ulong label_idx) const;  // needs filling out
+    // Sequence Compile(ContextList& context, const ulong label_idx, const ulong multi_label_idx) const;  // needs filling out
+    // Sequence Compile(ContextList& context, const ulong label_idx, const std::vector<Sequence>& args) const; // needs filling out
+
+    Sequence Compile(ContextList& context, const Ket& label_ket) const;
+    Sequence Compile(ContextList& context, const Ket& label_ket, const Ket& multi_label_ket) const;
+    Sequence Compile(ContextList& context, const Ket& label_ket, const std::vector<Sequence>& args) const;
+};
+
