1516d262d607686653225890e2262a79e580230c
Garry Morrison <garry@semantic-db.org>
2023-06-16 16:15:12 +0930

    Basic highlighting is in place. Still buggy and more features need adding!

--- src/GUI/Interface/LexerTextCtrl.cpp
+++ src/GUI/Interface/LexerTextCtrl.cpp
@@ -22,10 +22,26 @@ LexerTextCtrl::LexerTextCtrl(wxWindow* parent, wxWindowID id, const wxString& te
     // Enable and configure line numbers:
     SetMarginType(1, wxSTC_MARGIN_NUMBER);
     SetMarginWidth(1, 30);
+
+    // Load Lexer styles:
+    LoadLexerStyles();
+
+    // Load our sets of operator names:
+    LoadOperatorMaps();
+
+    // If using highlighting, let's highlight the starting text:
+    if (m_use_highlighting)
+    {
+        SyntaxHighlight(0, GetTextLength(), text.ToStdString());
+    }
+
+    // Bind to our Styling method:
+    Bind(wxEVT_STC_STYLENEEDED, &LexerTextCtrl::OnStyleNeeded, this);
 }
 
 void LexerTextCtrl::LoadLexerStyles()
 {
+    /*
     m_style_map[LEX::LEX_NONE] = *wxBLACK;
     m_style_map[LEX::LEX_LITERAL] = *wxBLUE;
     m_style_map[LEX::LEX_SIMPLE] = *wxBLUE;
@@ -35,6 +51,18 @@ void LexerTextCtrl::LoadLexerStyles()
     m_style_map[LEX::LEX_KET] = *wxBLUE;
     m_style_map[LEX::LEX_COMMENT] = *wxBLUE;
     m_style_map[LEX::LEX_STRING] = *wxBLUE;
+    */
+
+    // Map LEX objects to foreground colours:
+    StyleSetForeground(static_cast<int>(LEX::LEX_NONE), *wxBLACK);
+    StyleSetForeground(static_cast<int>(LEX::LEX_LITERAL), *wxLIGHT_GREY);
+    // StyleSetForeground(static_cast<int>(LEX::LEX_SIMPLE), *wxBLUE);
+    StyleSetForeground(static_cast<int>(LEX::LEX_SIMPLE), *wxRED);
+    StyleSetForeground(static_cast<int>(LEX::LEX_COMPOUND), *wxBLUE);
+    StyleSetForeground(static_cast<int>(LEX::LEX_FUNCTION), *wxBLUE);
+    StyleSetForeground(static_cast<int>(LEX::LEX_KET), *wxBLUE);
+    StyleSetForeground(static_cast<int>(LEX::LEX_COMMENT), *wxBLUE);
+    StyleSetForeground(static_cast<int>(LEX::LEX_STRING), *wxBLUE);
 
 }
 
@@ -58,14 +86,138 @@ void LexerTextCtrl::LoadOperatorMaps()
     }
 }
 
-void LexerTextCtrl::SyntaxHighlight(size_t start, size_t end, const wxString& text)
+// void LexerTextCtrl::SyntaxHighlight(size_t start, size_t end, const wxString& text)
+void LexerTextCtrl::SyntaxHighlight(size_t start, size_t end, const std::string& text)
 {
+    std::vector<LEX_OBJECT> lex_objects;
+    size_t text_pos = 0;
+    size_t start_pos = 0;
+    size_t end_of_text = text.size();
+    size_t start_token = 0;
+    bool inside_ket = false;
+    bool inside_object = false;
+    char c;
+    std::string token;
+    LEX_OBJECT object;
+    while (text_pos < end_of_text)  // This approach vs using for (const char c: string) ?
+    {
+        c = text[text_pos];
+        
+        if (c == '|')
+        {
+            if (inside_object)
+            {
+                object.LEX_ID = LEX::LEX_SIMPLE;
+                object.end = text_pos - 1;
+                lex_objects.push_back(object);
+                inside_object = false;
+            }
+            object.LEX_ID = LEX::LEX_KET;
+            object.start = text_pos;
+            inside_ket = true;
+        }
+        else if (inside_ket && c == '>')
+        {
+            object.end = text_pos + 1;
+            lex_objects.push_back(object);
 
+            // Reset object:  // Do we even need this?
+            // object.LEX_ID = LEX::LEX_NONE;
+            // object.start = 0;
+            // object.end = 0;
+
+            // Switch off inside_ket:
+            inside_ket = false;
+            // token.Clear();  // Shouldn't be needed
+        }
+        else if (!inside_ket && !inside_object && ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')))
+        {
+            object.start = text_pos;
+            inside_object = true;
+        }
+        else if (!inside_ket && inside_object && (c == ' ' || c == ',' || c == ']' || c == ')' || c == '[' || c == '('))
+        {
+            object.end = text_pos;
+            if (c == ' ' || c == ',' || c == ']' || c == ')')
+            {
+                // wxString op = text.Mid(object.start, text_pos - object.start);
+                std::string op = text.substr(object.start, text_pos - object.start);
+                if (m_simple.find(op) != m_simple.end())
+                {
+                    object.LEX_ID = LEX::LEX_SIMPLE;
+                }
+                else
+                {
+                    object.LEX_ID = LEX::LEX_LITERAL;
+                }
+            }
+            else if (c == '[')
+            {
+                object.LEX_ID = LEX::LEX_COMPOUND;
+            }
+            else if (c == '(')
+            {
+                object.LEX_ID = LEX::LEX_FUNCTION;
+            }
+            lex_objects.push_back(object);
+
+            // Do we need to clear object here? Possibly not, if we do it right.
+            // object.LEX_ID = LEX::LEX_NONE;
+            // object.start = 0;
+            // object.end = 0;
+
+            inside_object = false;
+        }
+        /*
+        else if (!inside_ket)
+        {
+            
+
+            token.Append(c);
+        }
+        */
+
+        text_pos++;  // Move on to next char position
+    }
+
+    // Remove the old styling:  // This is buggy for now, so leave it out
+    // StartStyling(start);
+    // SetStyling(end - start, 0);
+
+    // Style the lex objects:
+    for (LEX_OBJECT object : lex_objects)
+    {
+        StartStyling(object.start);
+        SetStyling(object.end - object.start, static_cast<int>(object.LEX_ID));
+    }
 }
 
-void LexerTextCtrl::OnStyleNeeded(wxStyledTextEvent& event)
+void LexerTextCtrl::OnStyleNeeded(wxStyledTextEvent& event)  // Buggy on update, for some reason!
 {
-    event.Skip();
+    size_t line_end = LineFromPosition(GetCurrentPos());
+    size_t line_start = LineFromPosition(GetEndStyled());
+
+    // if it is on one page, style the whole document:
+    if (GetLineCount() == LinesOnScreen())
+    {
+        line_start = 0;
+        line_end = GetLineCount() - 1;
+    }
+
+    if (line_end < line_start)
+    {
+        std::swap(line_start, line_end);
+    }
+
+    // Now find positions:
+    size_t start_pos = PositionFromLine(line_start);
+    size_t end_pos = GetLineEndPosition(line_end);
+    std::string local_text = GetTextRange(start_pos, end_pos).ToStdString();
+
+    // Now highlight it all:
+    this->SyntaxHighlight(start_pos, end_pos, local_text);
+
+    // event.Skip();   // Is this needed?
 }
 
 LexerTextCtrl::~LexerTextCtrl()
--- src/GUI/Interface/LexerTextCtrl.h
+++ src/GUI/Interface/LexerTextCtrl.h
@@ -28,7 +28,7 @@ enum class LEX : int {
 };
 
 struct LEX_OBJECT {
-	int LEX_ID = 0;
+	LEX LEX_ID = LEX::LEX_NONE;
 	size_t start = 0;
 	size_t end = 0;
 };
@@ -41,19 +41,20 @@ public:
 	void LoadLexerStyles();
 	void LoadOperatorMaps();
 
-	void SyntaxHighlight(size_t start, size_t end, const wxString& text);
+	// void SyntaxHighlight(size_t start, size_t end, const wxString& text);
+	void SyntaxHighlight(size_t start, size_t end, const std::string& text);
 	void OnStyleNeeded(wxStyledTextEvent& event);
 
 	~LexerTextCtrl();
 
 private:
+	bool m_use_highlighting = true;  // Apply highlighting by default, potentially add a checkbox to the EditFrame to enable/disable.
+
 	std::map<LEX, wxColor> m_style_map;
 	std::set<std::string> m_simple;
 	std::set<std::string> m_compound;
 	std::set<std::string> m_function;
 	std::set<std::string> m_keyword;
-
-	int m_sytle_mask = 0; // For wxWidgets > 3.1.0 this needs to be 0.
 
 };
 
